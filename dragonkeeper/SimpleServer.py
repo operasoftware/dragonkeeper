c7306376ba683bbbac96c496c0e5cb60d0386f85
Chris K <chrisk@opera.com>
2010-03-22 13:36:20 +0100

    An other try to fix for DFL-1026; Clean shutdown of Dragonkeeper

--- dragonkeeper/SimpleServer.py
+++ dragonkeeper/SimpleServer.py
@@ -10,6 +10,7 @@ class SimpleServer(asyncore.dispatcher):
     def __init__(self, host, port, connection_class, context):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.bind((host, port))
         self.listen(5)
         self.connection_class = connection_class
