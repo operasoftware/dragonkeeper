38ef7ac0da9acec6fc312048536a086a0a44d0eb
Chris K <chrisk@opera.com>
2012-07-11 11:14:43 +0200

    Fixed setup script.

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
-from dragonkeeper.common import __version__ as VERSION 
+from dragonkeeper.common import __version__ as VERSION
 
 try:
     from setuptools import setup, find_packages
     addl_args = dict(
             packages = find_packages(),
-            entry_points = {        
+            entry_points = {
             'console_scripts': [
                 'dragonkeeper = dragonkeeper.dragonkeeper:main_func'
                 ],
             },
         )
-        
+
 except ImportError:
     from distutils.core import setup
     addl_args = dict(
@@ -20,7 +20,7 @@ except ImportError:
             'dragonkeeper',
             ],
         )
-        
+
 setup(
     name='dragonkeeper',
     version=VERSION,
@@ -30,6 +30,6 @@ setup(
     data_files=[('dragonkeeper', ['dragonkeeper/favicon.ico']),
                 ('dragonkeeper', ['dragonkeeper/device-favicon.png'])],
     description='An HTTP proxy for Opera Dragonfly development',
-    long_description=open("README").read(),
+    long_description=open("README.md").read(),
     **addl_args
     )
